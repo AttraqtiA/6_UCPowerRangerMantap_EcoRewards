85fa9cf3d2e5b4a65432040e9b9764dba03d5a23
yebology <yobelnathaniel12@gmail.com>
2024-04-27 00:35:34 +0700

    admin topbar otw

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.admin.frame_admin');
 });
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
