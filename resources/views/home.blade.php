5e8c0ae5da31ce3eccb6c672c5bba360b4531a92
Bridgetbtrxc <bbeatrix@student.ciputra.ac.id>
2024-04-27 01:04:37 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-{{-- tes --}}
+{{-- tes lolol --}}
                         {{ __('You are logged in!') }}
                     </div>
                 </div>
