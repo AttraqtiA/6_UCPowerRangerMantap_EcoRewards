b2e93620ff07d2b162f425ff6169944791a9c642
yebology <yobelnathaniel12@gmail.com>
2024-04-27 21:25:09 +0700

    DONE VIEW MISSION YEAYY AKHIRREE

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 Route::get('/', function () {
-    return view('admin.waitingApprovalMission');
+    return view('user.viewMission');
 });
 
 Auth::routes();
